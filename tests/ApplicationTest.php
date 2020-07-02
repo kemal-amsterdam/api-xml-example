e50077bb2c808ea47473cfd3764c46623549e168
Kemal Djakman <kdjakman@gmail.com>
2020-07-02 13:20:21 +0200

    Add empty test classes

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ApplicationTest extends TestCase
+{
+    public function testCheckRequestMethod()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
---
+++ tests/Xml/Response/NackTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class NackTest extends TestCase
+{
+
+}
